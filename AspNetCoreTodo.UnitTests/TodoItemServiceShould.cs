6a529330a5a26d4180e512cdbe127f191a65cf29
Matt M <126421142+mein9@users.noreply.github.com>
2024-08-01 12:53:09 -0700

    test works

---
+++ AspNetCoreTodo.UnitTests/TodoItemServiceShould.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace AspNetCoreTodo.UnitTests
+{
+    public class TodoItemServiceShould
+    {
+        [Fact]
+        public async Task AddNewItemAsIncompleteWithDueDate()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+
+            // Set up a context (connection to the "DB") for writing
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+
+                // Change ApplicationUser to IdentityUser
+                var fakeUser = new IdentityUser
+                {
+                    Id = "fake-000",
+                    UserName = "fake@example.com"
+                };
+
+                await service.AddItemAsync(new TodoItem
+                {
+                    Title = "Testing?"
+                }, fakeUser);
+            }
+
+            // Use a separate context to read data back from the "DB"
+            using (var context = new ApplicationDbContext(options))
+            {
+                var itemsInDatabase = await context
+                    .Items.CountAsync();
+                Assert.Equal(1, itemsInDatabase);
+
+                var item = await context.Items.FirstAsync();
+                Assert.Equal("Testing?", item.Title);
+                Assert.Equal(false, item.IsDone);
+
+                // Item should be due 3 days from now (give or take a second)
+                var difference = DateTimeOffset.Now.AddDays(3) - item.DueAt;
+                Assert.True(difference < TimeSpan.FromSeconds(1));
+            }   // P. 103-104
+        }
+    }
+}
